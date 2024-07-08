321f04ad8741531b6defa083d69c54ef45064c95
sant-El <eliasvsr1@gmail.com>
2024-07-08 18:14:25 -0300

    exclusão de new e teste

--- teste.js
+++
@@ -1,7 +0,0 @@
-const prompt = require("prompt-sync")();
-const areaCirculo = require (`./new`)
-
-
-let entrada = Number(prompt(`Digite o raio: `))
-let area = areaCirculo(entrada)
-console.log(area)
